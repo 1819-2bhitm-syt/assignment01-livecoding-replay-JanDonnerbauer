7270ca49bd0485116a681a5f6c1fa7791934c321
JanDonnerbauer <donnerbauerjan@gmail.com>
2018-10-20 14:38:59 +0200

    18_10_2018 value_reference_type_weitere_beispiele

--- bsp05/app.js
+++ bsp05/app.js
@@ -10,6 +10,7 @@ console.log("y = ",y);
 */
 
 //Reference Type
+/*
 let x = { value: 10};
 let y = x;
 
@@ -17,3 +18,24 @@ x.value = 20;
 
 console.log("x = ",x);
 console.log("y = ", y);
+*/
+
+//weiteres Beispiel 1
+let number = 10;
+
+function increase(number) {
+    number++;
+}
+
+increase(number);
+console.log(number);
+
+//weiteres Beispiel 2
+let obj = { value: 10};
+
+function increase1(obj) {
+    obj.value++;
+}
+
+increase1(obj);
+console.log(obj);
